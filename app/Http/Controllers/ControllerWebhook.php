c3ce78ff7fcdf8dbda7654a2db3c9e77304a943f
dsotoch <dsoto6155@gmail.com>
2023-07-01 13:33:12 -0500

    w2

--- app/Http/Controllers/ControllerWebhook.php
+++ app/Http/Controllers/ControllerWebhook.php
@@ -10,15 +10,29 @@ class ControllerWebhook extends Controller
     public function handle(Request $request)
     {
         // Procesa los datos del webhook
-        $payload = $request->all();
-        $verificationCode = $request->input('verification_code');
-        DB::table('webhook')->insert([
-            'codigo' => $verificationCode,
-        ]);
+        $payload = $request->input('type');
+        switch ($payload) {
+            case 'charge.failed':
+                dd($request);
+                break;
+            case 'charge.cancelled':
+                dd($request);
+                break;
+            case 'subscription.charge.failed':
+                dd($request);
+                break;
+
+            default:
+                DB::table('tabla')->insert([
+                    'codigo' => $request->input('verification_code')
+                ]);
+                return response()->json(['message' => 'Webhook received'], 200);
+                break;
+        }
         // Realiza acciones adicionales según los datos recibidos
         // ...
 
         // Devuelve una respuesta
-        return response()->json(['message' => 'Webhook received'],200);
+        return response()->json(['message' => 'Webhook received'], 200);
     }
 }
